517638c052c4b675e6fbe67ecc4caaa520dbbfb4
Pablo Mendoza <asdson5555@gmail.com>
2024-03-26 13:00:52 -0500

    Interfas

--- Controller.cs
+++ Controller.cs
@@ -19,14 +19,18 @@ namespace TiendaDeArmas
             Armas[] ArmasMeleeArray = new Armas[1];
             ArmasMeleeArray[0] = new ArmasInfo("Espada", 150, 25, 5, 0, "");
 
+            //inventario
+            List<Armas> inventario = new List<Armas>();
+
             //Interfas
             bool continueFlag = true;
 
             while (continueFlag)
             {
-                Console.WriteLine("Ingresa Tipo de arma que desea comprar");
+                Console.WriteLine("Escoge una opción");
                 Console.WriteLine("1. Distancia");
                 Console.WriteLine("2. Melee");
+                Console.WriteLine("3. Ver inventario");
 
                 int option = int.Parse(Console.ReadLine());
 
@@ -40,15 +44,46 @@ namespace TiendaDeArmas
                                 $"ocasiona {ArmasArray[i].DañoArma()} cada {ArmasArray[i].VelocidadAtaque()} " +
                                 $"segundos y el costo de las {ArmasArray[i].NameArmo} es {ArmasArray[i].Munición()}");
                         }
+                        int election = int.Parse(Console.ReadLine());
+                        switch (election)
+                        {
+                            case 1:
+                                inventario.Add(ArmasArray[0]);
+                                break;
+                            case 2:
+                                inventario.Add(ArmasArray[1]);
+                                break;
+                            default:
+                                Console.WriteLine("Opción invalida");
+                                break;
+                        }
                         break;
                     case 2:
-                        Console.WriteLine("Las armas melee disponibles son :");
+                        Console.WriteLine("Que arma melee comprará??");
                         for (int i = 0; i < ArmasMeleeArray.Length; i++)
                         {
                             Console.WriteLine($"{i + 1}. {ArmasMeleeArray[i].Name} tiene un valor de {ArmasMeleeArray[i].PrecioArma()}, " +
                                 $"ocasiona {ArmasMeleeArray[i].DañoArma()} cada {ArmasMeleeArray[i].VelocidadAtaque()} segundos");
                         }
+                        int election2 = int.Parse(Console.ReadLine());
+                        switch (election2)
+                        {
+                            case 1:
+                                inventario.Add(ArmasMeleeArray[0]);
+                                break;
+                            default:
+                                Console.WriteLine("Opción invalida");
+                                break;
+                        }
                         break;
+                    case 3:
+                        Console.WriteLine("Las armas en tu inventario son: ");
+                        foreach(var Armas in inventario)
+                        {
+                            Console.WriteLine(Armas.Name);
+                        }
+                        break;
+
                     default:
                         Console.WriteLine("Opción inválida");
                         break;
